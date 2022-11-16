f95b24392fbed251cc1ba380baa86a61fc6aa39e
Muzhi Zhang <zmz0072000@outlook.com>
2022-11-16 10:41:49 -0800

    Script from d3 repo

---
+++ docs/script/b1.js
@@ -0,0 +1,264 @@
+//Apply margin to svg
+//Studied from Bhumika Srinivas' Starbucks Website example.
+const margin = {l: 80, r:50, t:80, b:50}
+const overall_width = 800
+const overall_height = 500
+const svg_name = "#b1"
+let outerSvg = d3.select(svg_name)
+    .append("svg")
+    .attr("width", overall_width + margin.l + margin.r)
+    .attr("height", overall_height + margin.t + margin.b);
+//Background
+outerSvg.append("rect")
+    .attr("width", "100%")
+    .attr("height", "100%")
+    .attr("fill", "#E6E6FA");
+let svg = outerSvg.append("g")
+    .attr("transform", `translate(${margin.l}, ${margin.t})`);
+
+
+function main(svg) {
+
+    let graph = svg.append("g").attr('class', 'graph');
+    //Variables, minus margin to prevent out of bound bars
+    //studied from: https://github.com/markumreed/data_science_for_everyone/blob/main/d3_project/bar_chart_csv/example.js
+    const width = overall_width;
+    const height = overall_height;
+    const legendLocation = [width - 130, 0];
+    const legend2Location = [width - 130, 100];
+
+    //Config
+    const url = "covid2.csv"
+    //const timeFormat = d3.utcFormat("%Y/%m/%d")
+
+
+    //const country/region info
+    const continents = ["Asia", "Europe", "North America"];
+    const colors = [
+        "rgb(189,32,43)",
+        "rgb(255,204,0)",
+        "rgb(10,49,97)"
+    ];
+    const classKeys = [];
+    for(let i = 0; i < continents.length; i++){
+        classKeys.push("c"+i.toString());
+    }
+    let continent2Class = d3.scaleOrdinal().domain(continents).range(classKeys);
+
+    //Const Data Key info
+    const keys = ["Cases per Million","Deaths per Million","People Vaccined per Hundred","People Fully Vaccined per Hundred"]
+
+
+    //Build scale for continents
+    let scaleContinentColor = d3.scaleOrdinal().domain(continents).range(colors);
+    //Scale building/mapping and axis drawing studied from: https://github.com/markumreed/data_science_for_everyone/blob/main/d3_project/bar_chart_csv/example.js
+    let scaleX = d3.scaleBand().range([0, width]).padding(0.5);
+    scaleX.domain(keys);
+
+    let scaleYs = {}
+    let axisYs = {}
+    for(let i = 0; i < keys.length; i++){
+        scaleYs[i] = d3.scaleLinear().range([height, 0]);
+        axisYs[i] = d3.axisLeft(scaleYs[i]);
+    }
+
+
+
+
+    d3.csv(url).then( function(data) {
+
+        for(let i = 0; i < keys.length; i++){
+            let key = keys[i];
+            let minY = 0;
+
+            let maxY = d3.max(
+                data, function(d) {return parseFloat(d[key])}
+            );
+            scaleYs[i].domain([minY, maxY]).nice();
+        }
+
+
+
+        //title
+        graph.append("text")
+            .attr("x", width / 2)
+            .attr("y", -40)
+            .attr('text-anchor', 'middle')
+            .attr('stroke', 'black')
+            .attr('font-weight', 600)
+            .text("Covid Data in Different Countries @ 09-01-2022");
+
+        let yAxis = graph.append("g")
+            .attr('class', 'yAxis')
+
+        for(let i = 0; i < keys.length; i++){
+            yAxis.append("g")
+                .attr("transform", `translate(${scaleX(keys[i])},0)`)
+                .call(axisYs[i])
+                .append("text")
+                .attr("y", height+20)
+                .attr("x", 0)
+                .attr('text-anchor', 'middle')
+                .attr('stroke', 'black')
+                .text(keys[i]);
+        }
+
+
+        let countries = [];
+        let countryColors = [];
+        let countryIds = [];
+        let count = 0;
+
+        data.forEach(function(d) {
+            let country = d["Country"];
+            let continent = d["Continent"];
+
+            let color = scaleContinentColor(d["Continent"]);
+            if(country === "Singapore"){
+                color = "rgb(0,132,61)"
+            }
+
+            countries.push(country);
+            countryColors.push(color);
+            let path = d3.path();
+            for(let i = 0; i < keys.length; i++){
+                let x = scaleX(keys[i]);
+                let y = scaleYs[i](d[keys[i]]);
+                if(i===0){
+                    path.moveTo(x, y);
+                } else {
+                    path.lineTo(x, y);
+                }
+            }
+            graph.append("path")
+                .attr("d", path)
+                .attr("id", function (){
+                    let id = "i"+count.toString();
+                    countryIds.push(id);
+                    count++;
+                    return id;
+                })
+                .attr("class", continent2Class(continent))
+                .attr("classHidden", false)
+                .attr("idHidden", false)
+                .attr("stroke", color)
+                .attr("stroke-width", 3)
+                .style("opacity", 0.5)
+                .attr("fill", "none");
+        })
+
+        //Build id scale
+        let country2id = d3.scaleOrdinal().domain(countries).range(countryIds);
+
+        //Build legend
+        let scaleCountryColor = d3.scaleOrdinal().domain(countries).range(countryColors);
+        //color
+        let legend1 = d3.legendColor()
+            .shape("path", d3.symbol().type(d3.symbolsStroke).size(150))
+            .shapePadding(10)
+            .scale(scaleContinentColor)
+            .on('cellclick', function (d){
+                clickClassCell(d, d3.select(this), graph, continent2Class);
+            });
+
+        let legend2 = d3.legendColor()
+            .shape("path", d3.symbol().type(d3.symbolsStroke).size(150))
+            .shapePadding(10)
+            .scale(scaleCountryColor)
+            .on('cellclick', function (d){
+                clickIdCell(d, d3.select(this), graph, country2id);
+            });
+
+        graph.append("g")
+            .attr("transform", `translate(${legendLocation[0]}, ${legendLocation[1]})`)
+            .call(legend1);
+        graph.append("g")
+            .attr("transform", `translate(${legend2Location[0]}, ${legend2Location[1]})`)
+            .call(legend2);
+    })
+}
+
+function hideById(e){
+    e.setAttribute('idHidden', true);
+    e.setAttribute('visibility', "hidden");
+}
+
+function hideByClass(e){
+    e.setAttribute('classHidden', true);
+    e.setAttribute('visibility', "hidden");
+}
+
+function showById(e){
+    e.setAttribute('idHidden', false);
+    if(e.getAttribute('classHidden')==="false"){
+        e.setAttribute('visibility', "visible");
+    }
+}
+
+function showByClass(e){
+    e.setAttribute('classHidden', false);
+    if(e.getAttribute('idHidden')==="false"){
+        e.setAttribute('visibility', "visible");
+    }
+}
+
+function clickIdCell(elem, cell, graph, scaleId){
+    let text = elem.target.textContent;
+    let id = scaleId(text);
+    let hidden;
+
+
+    if(cell.attr("clicked") === "false"){
+        cell.attr("clicked", "true")
+        cell.attr("text-decoration", "line-through");
+        hidden = true;
+    }
+    else{
+        cell.attr("clicked", "false")
+        cell.attr("text-decoration", "");
+        hidden = false;
+    }
+
+    graph.selectAll(`#${id}`)
+        .each(function() {
+            if(hidden){
+                hideById(this);
+            }
+            else{
+                showById(this);
+            }
+
+        })
+}
+
+function clickClassCell(elem, cell, graph, scaleClass){
+    let text = elem.target.textContent;
+    let className = scaleClass(text);
+    let hidden;
+
+
+    if(cell.attr("clicked") === "false"){
+        cell.attr("clicked", "true")
+        cell.attr("text-decoration", "line-through");
+        hidden = true;
+    }
+    else{
+        cell.attr("clicked", "false")
+        cell.attr("text-decoration", "");
+        hidden = false;
+    }
+
+    graph.selectAll(`.${className}`)
+        .each(function() {
+            if(hidden){
+                hideByClass(this);
+            }
+            else{
+                showByClass(this);
+            }
+
+        })
+}
+
+
+main(svg);
---
+++ docs/script/c1.js
@@ -0,0 +1,178 @@
+//Apply margin to svg
+//Studied from Bhumika Srinivas' Starbucks Website example.
+const margin = {l: 80, r:100, t:80, b:30}
+const overall_width = 700
+const overall_height = 500
+const svg_name = "#c1"
+let outerSvg = d3.select(svg_name)
+    .append("svg")
+    .attr("width", overall_width + margin.l + margin.r)
+    .attr("height", overall_height + margin.t + margin.b);
+//Background
+outerSvg.append("rect")
+    .attr("width", "100%")
+    .attr("height", "100%")
+    .attr("fill", "#E6E6FA");
+let svg = outerSvg.append("g")
+    .attr("transform", `translate(${margin.l}, ${margin.t})`);
+
+function main(svg) {
+
+
+    //background
+    let graph = svg.append("g").attr('class', 'graph');
+
+
+    //Config
+    const url = "labor.csv";
+
+    //Variables, minus margin to prevent out of bound bars
+    //studied from: https://github.com/markumreed/data_science_for_everyone/blob/main/d3_project/bar_chart_csv/example.js
+    const width = overall_width;
+    const height = overall_height;
+    const legendLocation = [width - 50, 230];
+
+    const keys = ["S Pass","Work Permit"];
+    const colors = [
+        "rgb(88,151,210)",
+        "rgb(210,183,52)"
+    ];
+    let colorScale = d3.scaleOrdinal().domain(keys).range(colors);
+
+    let scaleX = d3.scaleBand().range([0, width]).padding(0.5);
+    let scaleY = d3.scaleLinear().range([0, height]);
+    let realScaleY = d3.scaleLinear().range([height, 0]);
+    let axisX = d3.axisBottom(scaleX);
+    let axisY = d3.axisLeft(realScaleY);
+
+    d3.csv(url).then( function(data) {
+
+        //Map domain
+        scaleX.domain(data.map(function(d) {return d['Year']}));
+        let max = d3.max(data, function(d) {
+            let sum = 0;
+            for(let i = 0; i < keys.length; i++){
+                sum += parseInt(d[keys[i]]);
+            }
+            return sum;
+        }) + 100000;
+        scaleY.domain([0, max]).nice();
+        realScaleY.domain([0, max]).nice();
+
+        graph.append("text")
+            .attr("x", width / 2)
+            .attr("y", -50)
+            .attr('text-anchor', 'middle')
+            .attr('stroke', 'black')
+            .text("Work Permit and Special Work Pass (IT related)");
+
+        graph.append("text")
+            .attr("x", width / 2)
+            .attr("y", -50)
+            .attr("dy", "1.5em")
+            .attr('text-anchor', 'middle')
+            .attr('stroke', 'black')
+            .text("issued by Singapore");
+
+
+        graph.append("g")
+            .attr("transform", `translate(0,${height})`)
+            .call(axisX)
+            .append("text")
+            .attr("x", width/2)
+            .attr("y", 40)
+            .attr('fill', 'black')
+            .attr('text-anchor', 'middle')
+            .text("Fiscal Year");
+
+        graph.append("g")
+            .call(axisY)
+            .append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", -50)
+            .attr("x", -height/2)
+            .attr('text-anchor', 'middle')
+            .attr('fill', 'black')
+            .text('Work Visa granted');
+
+
+        //Legend format copied from
+        //https://d3-legend.susielu.com/
+        let legendFunc = d3.legendColor()
+            .shape("circle")
+            .shapePadding(10)
+            .title("Visa Type")
+            .scale(colorScale);
+        svg.append("g")
+            .attr("transform", `translate(${legendLocation[0]}, ${legendLocation[1]})`)
+            .attr("class", "legend")
+            .attr('stroke', 'none')
+            .call(legendFunc);
+
+        //Bar drawing studied from: https://learning.oreilly.com/library/view/interactive-data-visualization/9781491921296/ch06.html#idm140093202999712
+        let bars1 = graph.selectAll("rect2")
+            .data(data)
+            .enter();
+
+        bars1.append("rect")
+            .attr("class", "bar")
+            .attr("x", function(d) {return scaleX(d['Year'])})
+            .attr("y", function(d) {
+                let v = parseInt(d[keys[0]]) + parseInt(d[keys[1]]);
+                return height - scaleY(v)
+            })
+            .attr("width", scaleX.bandwidth())
+            .attr("height", function(d) {
+                let v = parseInt(d[keys[0]]) + parseInt(d[keys[1]]);
+                return scaleY(v);
+            })
+            .attr('fill', function() {
+                //calculate color using sequential hue in d3
+                return colorScale(colorScale(keys[1]));
+            })
+            .attr("stroke", "black");
+        bars1.append("text")
+            .attr("x", function(d) {return scaleX(d['Year']) + scaleX.bandwidth() / 2})
+            .attr("y", function(d) {
+                let v = parseInt(d[keys[0]]) + parseInt(d[keys[1]]);
+                return height - scaleY(v) + 15
+            })
+            .attr('text-anchor', 'middle')
+            .style("font-size", "12px")
+            .text(function(d) {return d[keys[1]]})
+
+        let bars2 = graph.selectAll("rect2")
+            .data(data)
+            .enter();
+        bars2.append("rect")
+            .attr("class", "bar")
+            .attr("x", function(d) {return scaleX(d['Year'])})
+            .attr("y", function(d) {
+                let v = parseInt(d[keys[0]]);
+                return height - scaleY(v)
+            })
+            .attr("width", scaleX.bandwidth())
+            .attr("height", function(d) {
+                let v = parseInt(d[keys[0]]);
+                return scaleY(v);
+            })
+            .attr('fill', function() {
+                //calculate color using sequential hue in d3
+                return colorScale(colorScale(keys[0]));
+            })
+            .attr("stroke", "black");
+        bars2.append("text")
+            .attr("x", function(d) {return scaleX(d['Year']) + scaleX.bandwidth() / 2})
+            .attr("y", function(d) {
+                let v = parseInt(d[keys[0]]);
+                return height - scaleY(v) + 15
+            })
+            .attr('text-anchor', 'middle')
+            .style("font-size", "12px")
+            .text(function(d) {return d[keys[0]]})
+
+
+    })
+}
+
+main(svg);
