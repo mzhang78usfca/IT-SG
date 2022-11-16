a16257c4702a805f591ee69553fba4156ffb6d46
Muzhi Zhang <zmz0072000@outlook.com>
2022-11-16 11:40:43 -0800

    Final Ver

--- docs/script/d1.js
+++ docs/script/d1.js
@@ -1,137 +1,143 @@
-//Apply margin to svg
+function d1() {
+    //Apply margin to svg
 //Studied from Bhumika Srinivas' Starbucks Website example.
-const margin = {l: 80, r:50, t:80, b:30}
-const overall_width = 800
-const overall_height = 500
-const svg_name = "#d1"
-let outerSvg = d3.select(svg_name)
-    .append("svg")
-    .attr("width", overall_width + margin.l + margin.r)
-    .attr("height", overall_height + margin.t + margin.b);
+    const margin = {l: 80, r:50, t:80, b:30}
+    const overall_width = 800
+    const overall_height = 500
+    const svg_name = "#d1"
+    let outerSvg = d3.select(svg_name)
+        .append("svg")
+        .attr("width", overall_width + margin.l + margin.r)
+        .attr("height", overall_height + margin.t + margin.b);
 //Background
-outerSvg.append("rect")
-    .attr("width", "100%")
-    .attr("height", "100%")
-    .attr("fill", "#E6E6FA");
-let svg = outerSvg.append("g")
-    .attr("transform", `translate(${margin.l}, ${margin.t})`);
+    outerSvg.append("rect")
+        .attr("width", "100%")
+        .attr("height", "100%")
+        .attr("fill", "#E6E6FA");
+    let svg = outerSvg.append("g")
+        .attr("transform", `translate(${margin.l}, ${margin.t})`);
 
-function main(svg) {
+    function main(svg) {
 
-    let graph = svg.append("g").attr('class', 'graph');
-
-
-    //Config
-    const url = "revenue.csv"
-    const timeFormat = d3.utcFormat("%Y")
-
-    //const country/region info
-    const names = ["GDP", "Info Comm Industry Revenue"];
-    const keys = ["gdp", "revenue"]
-    const colors = [
-        "rgb(32,124,72)",
-        "rgb(30,142,168)",
-    ];
-
-    //Variables, minus margin to prevent out of bound bars
-    //studied from: https://github.com/markumreed/data_science_for_everyone/blob/main/d3_project/bar_chart_csv/example.js
-    const width = overall_width;
-    const height = overall_height;
-    const legendLocation = [width - 200, 230];
-
-    //Scale building/mapping and axis drawing studied from: https://github.com/markumreed/data_science_for_everyone/blob/main/d3_project/bar_chart_csv/example.js
-    let scaleX = d3.scaleTime().range([0, width]);
-    let scaleY = d3.scaleLinear().range([height, 0]);
-    let axisX = d3.axisBottom(scaleX);
-    let axisY = d3.axisLeft(scaleY);
-    let scaleColor = d3.scaleOrdinal().domain(names).range(colors);
+        let graph = svg.append("g").attr('class', 'graph');
 
 
+        //Config
+        const url = "revenue.csv"
+        const timeFormat = d3.utcFormat("%Y")
 
-    d3.csv(url).then( function(data) {
+        //const country/region info
+        const names = ["GDP", "Info Comm Industry Revenue"];
+        const keys = ["gdp", "revenue"]
+        const colors = [
+            "rgb(32,124,72)",
+            "rgb(30,142,168)",
+        ];
 
-        //parse date
-        let parse = d3.timeParse(timeFormat);
-        data.forEach(function(d) {
-            d["date"] = parse(d["year"]);
+        //Variables, minus margin to prevent out of bound bars
+        //studied from: https://github.com/markumreed/data_science_for_everyone/blob/main/d3_project/bar_chart_csv/example.js
+        const width = overall_width;
+        const height = overall_height;
+        const legendLocation = [width - 200, 230];
+
+        //Scale building/mapping and axis drawing studied from: https://github.com/markumreed/data_science_for_everyone/blob/main/d3_project/bar_chart_csv/example.js
+        let scaleX = d3.scaleTime().range([0, width]);
+        let scaleY = d3.scaleLinear().range([height, 0]);
+        let axisX = d3.axisBottom(scaleX);
+        let axisY = d3.axisLeft(scaleY);
+        let scaleColor = d3.scaleOrdinal().domain(names).range(colors);
+
+
+
+        d3.csv(url).then( function(data) {
+
+            //parse date
+            let parse = d3.timeParse(timeFormat);
+            data.forEach(function(d) {
+                d["date"] = parse(d["year"]);
+            })
+
+
+            //Map domain
+            //X
+            let minX = d3.min(data, function(d) {return d["date"]});
+            let maxX = d3.max(data, function(d) {return d["date"]});
+            scaleX.domain([minX, maxX]).nice();
+            axisX.ticks(d3.timeYear.every(1));
+
+            //Y
+            let minY = d3.min((data), function(d) {return Math.min(
+                parseFloat(d[keys[0]]),
+                parseFloat(d[keys[1]]),
+            )});
+            let maxY = d3.max((data), function(d) {return Math.max(
+                parseFloat(d[keys[0]]),
+                parseFloat(d[keys[1]]),
+            )});
+            scaleY.domain([minY, maxY]).nice();
+
+            //color
+            let legend = d3.legendColor()
+                .shape("path", d3.symbol().type(d3.symbolsStroke).size(150))
+                .shapePadding(10)
+                .scale(scaleColor)
+
+
+            //title
+            graph.append("text")
+                .attr("x", width / 2)
+                .attr("y", -10)
+                .attr('text-anchor', 'middle')
+                .attr('stroke', 'black')
+                .attr('font-weight', 600)
+                .text("Singapore GDP and IT revenue changes during COVID pandemic");
+            graph.append("g")
+                .attr("transform", `translate(0,${height})`)
+                .call(axisX)
+                .attr('stroke', 'black')
+                .append("text")
+                .attr("x", width/2)
+                .attr("y", 40)
+                .attr('stroke', 'black')
+                .attr('text-anchor', 'middle')
+                .text("Time");
+            graph.append("g")
+                .attr("transform", `translate(${scaleX(minX)},0)`)
+                .call(axisY)
+                .attr('stroke', 'black')
+                .append("text")
+                .attr("transform", "rotate(-90)")
+                .attr("y", -50-scaleX(minX))
+                .attr("x", -height/2)
+                .attr('text-anchor', 'middle')
+                .attr('stroke', 'black')
+                .text('Income / $ Billion');
+
+            //legend
+            graph.append("g")
+                .attr("transform", `translate(${legendLocation[0]}, ${legendLocation[1]})`)
+                .call(legend);
+
+            for(let i = 0; i < names.length; i++){
+                let key = keys[i];
+                let color = colors[i];
+                graph.append("path")
+                    .datum(data)
+                    .attr("fill", "none")
+                    .attr("stroke-width", 2)
+                    .attr("stroke", color)
+                    .attr("d", d3.line()
+                        .x(function(d) {
+                            return scaleX(d['date'])})
+                        .y(function(d) {return scaleY(parseFloat(d[key]))}));
+            }
+
+
+
         })
+    }
 
+    main(svg);
 
-        //Map domain
-        //X
-        let minX = d3.min(data, function(d) {return d["date"]});
-        let maxX = d3.max(data, function(d) {return d["date"]});
-        scaleX.domain([minX, maxX]).nice();
-        axisX.ticks(d3.timeYear.every(1));
-
-        //Y
-        let minY = d3.min((data), function(d) {return Math.min(
-            parseFloat(d[keys[0]]),
-            parseFloat(d[keys[1]]),
-        )});
-        let maxY = d3.max((data), function(d) {return Math.max(
-            parseFloat(d[keys[0]]),
-            parseFloat(d[keys[1]]),
-        )});
-        scaleY.domain([minY, maxY]).nice();
-
-        //color
-        let legend = d3.legendColor()
-            .shape("path", d3.symbol().type(d3.symbolsStroke).size(150))
-            .shapePadding(10)
-            .scale(scaleColor)
-
-
-        //title
-        graph.append("text")
-            .attr("x", width / 2)
-            .attr("y", -10)
-            .attr('text-anchor', 'middle')
-            .attr('stroke', 'black')
-            .attr('font-weight', 600)
-            .text("Singapore GDP and IT revenue changes during COVID pandemic");
-        graph.append("g")
-            .attr("transform", `translate(0,${height})`)
-            .call(axisX)
-            .append("text")
-            .attr("x", width/2)
-            .attr("y", 40)
-            .attr('stroke', 'black')
-            .attr('text-anchor', 'middle')
-            .text("Time");
-        graph.append("g")
-            .attr("transform", `translate(${scaleX(minX)},0)`)
-            .call(axisY)
-            .append("text")
-            .attr("transform", "rotate(-90)")
-            .attr("y", -50-scaleX(minX))
-            .attr("x", -height/2)
-            .attr('text-anchor', 'middle')
-            .attr('stroke', 'black')
-            .text('Income / $ Billion');
-
-        //legend
-        graph.append("g")
-            .attr("transform", `translate(${legendLocation[0]}, ${legendLocation[1]})`)
-            .call(legend);
-
-        for(let i = 0; i < names.length; i++){
-            let key = keys[i];
-            let color = colors[i];
-            graph.append("path")
-                .datum(data)
-                .attr("fill", "none")
-                .attr("stroke-width", 2)
-                .attr("stroke", color)
-                .attr("d", d3.line()
-                    .x(function(d) {
-                        return scaleX(d['date'])})
-                    .y(function(d) {return scaleY(parseFloat(d[key]))}));
-        }
-
-
-
-    })
 }
-
-main(svg);
+d1();
--- docs/script/e1.js
+++ docs/script/e1.js
@@ -1,145 +1,152 @@
-//Apply margin to svg
+function e1() {
+    //Apply margin to svg
 //Studied from Bhumika Srinivas' Starbucks Website example.
-const margin = {l: 80, r:100, t:80, b:30}
-const overall_width = 750
-const overall_height = 500
-const svg_name = "#e1"
-let outerSvg = d3.select(svg_name)
-    .append("svg")
-    .attr("width", overall_width + margin.l + margin.r)
-    .attr("height", overall_height + margin.t + margin.b);
+    const margin = {l: 80, r:100, t:80, b:30}
+    const overall_width = 750
+    const overall_height = 500
+    const svg_name = "#e1"
+    let outerSvg = d3.select(svg_name)
+        .append("svg")
+        .attr("width", overall_width + margin.l + margin.r)
+        .attr("height", overall_height + margin.t + margin.b);
 //Background
-outerSvg.append("rect")
-    .attr("width", "100%")
-    .attr("height", "100%")
-    .attr("fill", "#E6E6FA");
-let svg = outerSvg.append("g")
-    .attr("transform", `translate(${margin.l}, ${margin.t})`);
+    outerSvg.append("rect")
+        .attr("width", "100%")
+        .attr("height", "100%")
+        .attr("fill", "#E6E6FA");
+    let svg = outerSvg.append("g")
+        .attr("transform", `translate(${margin.l}, ${margin.t})`);
 
-function main(svg) {
+    function main(svg) {
 
-    let graph = svg.append("g").attr('class', 'graph');
+        let graph = svg.append("g").attr('class', 'graph');
 
-    //Config
-    const url = "company.csv"
-    const timeFormat = d3.utcFormat("%Y/%m/%d")
-    const bubbleMax = 40;
+        //Config
+        const url = "company.csv"
+        const timeFormat = d3.utcFormat("%Y/%m/%d")
+        const bubbleMax = 40;
 
-    //Variables, minus margin to prevent out of bound bars
-    //studied from: https://github.com/markumreed/data_science_for_everyone/blob/main/d3_project/bar_chart_csv/example.js
-    const width = overall_width;
-    const height = overall_height;
-    const legendLoc = [width - 130, -50];
-
-
-
-    //Scale building/mapping and axis drawing studied from: https://github.com/markumreed/data_science_for_everyone/blob/main/d3_project/bar_chart_csv/example.js
-    let scaleX = d3.scaleTime().range([0, width]);
-    let scaleY = d3.scaleLinear().range([height, 0]);
-    let scaleV = d3.scaleLinear().range([0, bubbleMax]);
-    let axisX = d3.axisBottom(scaleX);
-    let axisY = d3.axisLeft(scaleY);
-
-
-    d3.csv(url).then( function(data) {
-
-        //parse date
-        let parse = d3.timeParse(timeFormat);
-        data.forEach(function(d) {
-            d["Date"] = parse(d["Move in Date"]);
-            d["Revenue"] = parseFloat(d["2020 Revenue / Million"]);
-        })
-
-        //Map domain
-        //X
-        let minX = d3.min(data, function(d) {return d["Date"]});
-        let maxX = d3.max(data, function(d) {return d["Date"]});
-        scaleX.domain([minX, maxX]).nice();
-
-        //Y
-        let minY = d3.min(data, function(d) {return d['Revenue']});
-        let maxY = d3.max(data, function(d) {return d['Revenue']});
-        scaleY.domain([minY, maxY]).nice();
-
-        //V
-        let minV = 0;
-        let maxV = d3.max(data, function(d) {return parseFloat(d['Size'])});
-        scaleV.domain([minV, maxV]).nice();
-
-        //Legend
-        let legend = d3.legendSize()
-            .scale(scaleV)
-            .shape('circle')
-            .shapePadding(15)
-            .labelOffset(20)
-            .orient('vertical')
-            .cellFilter(function(d){ return d.label !== '0.0'})
-            .title("Company Size / Employee");
-
-        let lg = svg.append('g')
-            .attr("transform", `translate(${legendLoc[0]},${legendLoc[1]})`)
-            .call(legend)
-            .attr("fill", "darkgreen");
+        //Variables, minus margin to prevent out of bound bars
+        //studied from: https://github.com/markumreed/data_science_for_everyone/blob/main/d3_project/bar_chart_csv/example.js
+        const width = overall_width;
+        const height = overall_height;
+        const legendLoc = [width - 130, -50];
 
 
 
+        //Scale building/mapping and axis drawing studied from: https://github.com/markumreed/data_science_for_everyone/blob/main/d3_project/bar_chart_csv/example.js
+        let scaleX = d3.scaleTime().range([0, width]);
+        let scaleY = d3.scaleLinear().range([height, 0]);
+        let scaleV = d3.scaleLinear().range([0, bubbleMax]);
+        let axisX = d3.axisBottom(scaleX);
+        let axisY = d3.axisLeft(scaleY);
 
 
-        //paint title
-        graph.append("text")
-            .attr("x", width / 2)
-            .attr("y", -50)
-            .attr('text-anchor', 'middle')
-            .attr('stroke', 'black')
-            .attr('font-weight', 600)
-            .text("IT Companies Moved to Singapore");
+        d3.csv(url).then( function(data) {
 
-
-        graph.append("g")
-            .attr("transform", `translate(0,${height})`)
-            .call(axisX)
-            .append("text")
-            .attr("x", width/2)
-            .attr("y", 40)
-            .attr('stroke', 'black')
-            .attr('text-anchor', 'middle')
-            .text("Time");
-
-        graph.append("g")
-            .attr("transform", `translate(${scaleX(minX)},0)`)
-            .call(axisY)
-            .append("text")
-            .attr("transform", "rotate(-90)")
-            .attr("y", -50-scaleX(minX))
-            .attr("x", -height/2)
-            .attr('text-anchor', 'middle')
-            .attr('stroke', 'black')
-            .text('Company Revenue in 2020');
-
-
-        let bubbles = graph.selectAll("circle")
-            .data(data)
-            .enter();
-
-        bubbles.append("circle")
-            .attr("class", "bubble")
-            .attr("cx", function(d) {return scaleX(d['Date'])})
-            .attr("cy", function(d) {return scaleY(d['Revenue'])})
-            .attr("r", function(d) {return scaleV(parseFloat(d['Size']))})
-            .attr('stroke', 'black')
-            .attr("fill", "steelblue");
-
-
-        bubbles.append("text")
-            .attr("x", function(d) {return scaleX(d['Date'])})
-            .attr("y", function(d) {
-                return (scaleY(d['Revenue']) - scaleV(parseFloat(d['Size']) + 100))
+            //parse date
+            let parse = d3.timeParse(timeFormat);
+            data.forEach(function(d) {
+                d["Date"] = parse(d["Move in Date"]);
+                d["Revenue"] = parseFloat(d["2020 Revenue / Million"]);
             })
-            .attr('text-anchor', 'middle')
-            .text(function(d) {return d['Company']})
+
+            //Map domain
+            //X
+            let minX = d3.min(data, function(d) {return d["Date"]});
+            let maxX = d3.max(data, function(d) {return d["Date"]});
+            scaleX.domain([minX, maxX]).nice();
+
+            //Y
+            let minY = d3.min(data, function(d) {return d['Revenue']});
+            let maxY = d3.max(data, function(d) {return d['Revenue']});
+            scaleY.domain([minY, maxY]).nice();
+
+            //V
+            let minV = 0;
+            let maxV = d3.max(data, function(d) {return parseFloat(d['Size'])});
+            scaleV.domain([minV, maxV]).nice();
+
+            //Legend
+            let legend = d3.legendSize()
+                .scale(scaleV)
+                .shape('circle')
+                .shapePadding(15)
+                .labelOffset(20)
+                .orient('vertical')
+                .cellFilter(function(d){ return d.label !== '0.0'})
+                .title("Company Size / Employee");
+
+            let lg = svg.append('g')
+                .attr("transform", `translate(${legendLoc[0]},${legendLoc[1]})`)
+                .call(legend)
+                .attr("fill", "darkgreen");
 
 
-    })
+
+
+
+            //paint title
+            graph.append("text")
+                .attr("x", width / 2)
+                .attr("y", -50)
+                .attr('text-anchor', 'middle')
+                .attr('stroke', 'black')
+                .attr('font-weight', 600)
+                .text("IT Companies Moved to Singapore");
+
+
+            graph.append("g")
+                .attr("transform", `translate(0,${height})`)
+                .call(axisX)
+                .attr('stroke', 'black')
+                .append("text")
+                .attr("x", width/2)
+                .attr("y", 40)
+                .attr('stroke', 'black')
+                .attr('text-anchor', 'middle')
+                .text("Time");
+
+            graph.append("g")
+                .attr("transform", `translate(${scaleX(minX)},0)`)
+                .call(axisY)
+                .attr('stroke', 'black')
+                .append("text")
+                .attr("transform", "rotate(-90)")
+                .attr("y", -50-scaleX(minX))
+                .attr("x", -height/2)
+                .attr('text-anchor', 'middle')
+                .attr('stroke', 'black')
+                .text('Company Revenue in 2020');
+
+
+            let bubbles = graph.selectAll("circle")
+                .data(data)
+                .enter();
+
+            bubbles.append("circle")
+                .attr("class", "bubble")
+                .attr("cx", function(d) {return scaleX(d['Date'])})
+                .attr("cy", function(d) {return scaleY(d['Revenue'])})
+                .attr("r", function(d) {return scaleV(parseFloat(d['Size']))})
+                .attr('stroke', 'black')
+                .attr("fill", "steelblue");
+
+
+            bubbles.append("text")
+                .attr("x", function(d) {return scaleX(d['Date'])})
+                .attr("y", function(d) {
+                    return (scaleY(d['Revenue']) - scaleV(parseFloat(d['Size']) + 100))
+                })
+                .attr('text-anchor', 'middle')
+                .text(function(d) {return d['Company']})
+
+
+        })
+    }
+
+    main(svg);
+
 }
 
-main(svg);
+e1();
