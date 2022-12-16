0738c1989328e379c338107fa89d3c9c5d99a971
Muzhi Zhang <zmz0072000@outlook.com>
2022-12-15 22:14:55 -0800

    E1 tooltip

--- docs/script/e1.js
+++ docs/script/e1.js
@@ -25,6 +25,13 @@ function e1(){
         .attr('font-size', "25px")
         .attr('font-weight', 600)
         .text("IT Companies Moved to Singapore");
+    svg.append("text")
+        .attr("x", outerWidth / 2)
+        .attr("y", 50)
+        .attr("dy", "1em")
+        .attr('text-anchor', 'middle')
+        .attr('font-size', "25px")
+        .text("(Click to navigate to related news)");
 
 
 
@@ -44,7 +51,10 @@ function e1(){
         //Config
         //const margin = {l: 120, r:300, t:80, b:80}
         const url = "file/company.csv"
+        const numberFormat = ","
+        let numberFormatFunc = d3.format(numberFormat)
         const timeFormat = d3.utcFormat("%Y/%m/%d")
+        const displayTimeFormat = d3.utcFormat("%b %d, %Y")
         const bubbleMax = 40;
         const legendLoc = [width+60, 150];
 
@@ -55,7 +65,7 @@ function e1(){
         let scaleY = d3.scaleLinear().range([height, 0]);
         let scaleV = d3.scaleLinear().range([0, bubbleMax]);
         let axisX = d3.axisBottom(scaleX);
-        let axisY = d3.axisLeft(scaleY);
+        let axisY = d3.axisLeft(scaleY).tickFormat(numberFormatFunc);
 
 
         d3.csv(url).then( function(data) {
@@ -99,10 +109,6 @@ function e1(){
                 .attr("transform", `translate(${legendLoc[0]},${legendLoc[1]})`)
                 .call(legend);
 
-
-
-
-
             graph.append("g")
                 .attr("transform", `translate(0,${height})`)
                 .attr('class', 'axis')
@@ -125,7 +131,7 @@ function e1(){
                 .attr('text-anchor', 'middle')
                 .attr('stroke', 'none')
                 .attr('fill', 'black')
-                .text('Company Revenue in 2020');
+                .text('Company Revenue in 2020 / Million USD');
 
 
             let bubbles = graph.selectAll("circle")
@@ -137,9 +143,18 @@ function e1(){
                 .attr("cx", function(d) {return scaleX(d['Date'])})
                 .attr("cy", function(d) {return scaleY(d['Revenue'])})
                 .attr("r", function(d) {return scaleV(parseFloat(d['Size']))})
+                .attr("date", function(d) {return displayTimeFormat(d['Date'])})
+                .attr("size", function(d) {return parseFloat(d['Size'])})
+                .attr("revenue", function(d) {return numberFormatFunc(d['Revenue'])})
+                .attr("company", function(d) {return d['Company']})
+                .attr("url", function(d) {return d['Link']})
                 .attr("opacity", "0.5")
+                .attr('stroke-width', 1)
                 .attr('stroke', 'black')
-                .attr("fill", "steelblue");
+                .attr("fill", "steelblue")
+                .on("click", click)
+                .on("mouseover", mouseOver)
+                .on("mouseout", mouseOut);
 
 
             bubbles.append("text")
@@ -151,8 +166,89 @@ function e1(){
                 .text(function(d) {return d['Company']})
 
 
+
+            //Generate info box
+            const tooltipConfig = {
+                x: width * 0.1,
+                y: height * 0.2,
+                width: 240,
+                height: 120,
+                textOffset: [15, 25]
+            }
+
+            let focus = graph.append("g")
+                .attr("class", "focus")
+                .attr("transform", `translate(${tooltipConfig.x}, ${tooltipConfig.y})`)
+                .style("display", "none");
+            focus.append("rect")
+                .attr("class", "tooltip")
+                .attr("stroke", "black")
+                .attr("fill", "white")
+                .attr("width", tooltipConfig.width)
+                .attr("height", tooltipConfig.height)
+                .attr("rx", 4)
+                .attr("ry", 4);
+            let textArea = focus.append("g")
+                .attr("transform", `translate(${tooltipConfig.textOffset[0]}, ${tooltipConfig.textOffset[1]})`);
+            textArea.append("text")
+                .attr("class", "tooltip-1")
+                .attr('font-weight', 600)
+                .attr("dy", "0em")
+                .text("");
+            textArea.append("text")
+                .attr("class", "tooltip-2")
+                .attr("dy", "1.5em")
+                .text("");
+            textArea.append("text")
+                .attr("class", "tooltip-3")
+                .attr("dy", "3em")
+                .text("");
+            textArea.append("text")
+                .attr("class", "tooltip-4")
+                .attr("dy", "4.5em")
+                .text("");
+
+
+            function click() {
+                let target = d3.select(this);
+                let url = target.attr("url");
+                //Open new window:
+                //https://stackoverflow.com/questions/37722130/d3-js-opening-a-new-tab-by-dbclick-event
+                window.open(
+                    url,
+                    '_blank'
+                );
+            }
+
+            function mouseOver(){
+                let target = d3.select(this);
+                //Change cursor:
+                //https://stackoverflow.com/questions/36326683/d3-js-how-can-i-set-the-cursor-to-hand-when-mouseover-these-elements-on-svg-co
+                target.attr('stroke-width', 2.5)
+                    .style("cursor", "pointer");
+                focus.select(".tooltip-1")
+                    .text(target.attr("company"));
+                focus.select(".tooltip-2")
+                    .text("Time: " + target.attr("date"));
+                focus.select(".tooltip-3")
+                    .text("Revenue: "+ target.attr("revenue") +" Million USD");
+                focus.select(".tooltip-4")
+                    .text("Office Size: " + target.attr("size") + " people");
+                focus.style("display", null)
+            }
+
+            function mouseOut() {
+                let target = d3.select(this);
+                target.attr('stroke-width', 1)
+                    .style("cursor", "default");
+                focus.style("display", "none")
+            }
+
+
         })
     }
+
+
 
     main(svg);
 
